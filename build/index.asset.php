4b82627fb5939d1aa8a92c5ab93ab2b74df49d6e
Alex <alexcu21@live.com>
2022-05-22 22:35:45 -0600

    adding test

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => 'ee34766923e033df117059615692e03c');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => 'b24d124a6b64ff7124d8');
