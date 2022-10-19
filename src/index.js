da2fdc3eaa6ff0e379039744573a067145a10f57
Alex Cuadra <alexcu21@live.com>
2022-10-18 21:07:51 -0600

    addong support to transforms

--- src/index.js
+++ src/index.js
@@ -18,5 +18,35 @@ registerBlockType( name, {
 	 * @see ./save.js
 	 */
 	save: () => null,
+
+	transforms: {
+		from: [
+			{
+				type: 'shortcode',
+				tag:'weather',
+				transform({attributes: {cityname}}){
+					return createBlock('create-block/weatherblock', {
+						cityName: cityname
+					})
+				}
+			},
+		],
+
+	},
+
+	// transforms: {
+	// 	from: [
+	// 		{
+	// 			type: 'block',
+	// 			blocks:['core/shortcode'],
+	// 			transform({attributes: {cityname}}){
+	// 				return createBlock('create-block/weatherblock', {
+	// 					cityName: cityname
+	// 				})
+	// 			}
+	// 		},
+	// 	],
+
+	// },
 	
 });
